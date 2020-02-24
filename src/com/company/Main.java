e82b3d381fda7251be314ce7a79f5c4219120668
Jason Fulks <jason.fulks.2023@student.lhschools.org>
2020-02-24 11:55:53 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-------------------------------+");
+        System.out.println("|           Taco Gas            |");
+        System.out.println("|     Date        Time          |");
+        System.out.println("|     1/10/2020   12:17 PM      |");
+        System.out.println("|8662 Aconite Drive             |");
+        System.out.println("|Blacklick                      |");
+        System.out.println("|Ohio                           |");
+        System.out.println("|Invoice Number#    675849389847|");
+        System.out.println("|Auth#              675849389847|");
+        System.out.println("|           MasterCard          |");
+        System.out.println("|        Amount Number          |");
+        System.out.println("|       ****************54      |");
+        System.out.println("|   Pump      Product    $/G    |");
+        System.out.println("|    G3        UNLD       2.69  |");
+        System.out.println("|   Gallons   Fuel       Total  |");
+        System.out.println("|   11.28              $ 30.34  |");
+        System.out.println("|    Thanks for Choosing Us!    |");
+        System.out.println("|           Taco Gas            |");
+        System.out.println("+-------------------------------+");
+    }
+
+}
